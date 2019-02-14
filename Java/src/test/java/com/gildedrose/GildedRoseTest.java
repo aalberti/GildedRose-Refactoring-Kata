f5794a7a31a0949fc9877600312d218e63479c47
aalberti <aa@antoinealberti.com>
2019-02-14 16:24:36 +0100

    extract Sulfuras

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -70,7 +70,7 @@ public class GildedRoseTest {
 
     @Test
     public void SulfurasBeingALegendaryItemNeverHasToBeSoldOrDecreasesInQuality() throws Exception {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 80) };
+        Item[] items = new Item[] {new Sulfuras(-1, 80)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         Item item = app.getItem("Sulfuras, Hand of Ragnaros");
