88dd50c9f9ad5513c7a8145b6d10bf91ccf1a6d7
aalberti <aa@antoinealberti.com>
2019-02-14 16:40:41 +0100

    override update in BackstagePasses

--- Java/src/main/java/com/gildedrose/BackstagePasses.java
+++ Java/src/main/java/com/gildedrose/BackstagePasses.java
@@ -4,4 +4,25 @@ public class BackstagePasses extends Item {
     public BackstagePasses(int sellIn, int quality) {
         super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
     }
+
+    @Override
+    void update() {
+        if (this.quality < 50) {
+            this.quality = this.quality + 1;
+
+            if (this.sellIn < 11 && this.quality < 50) {
+                this.quality = this.quality + 1;
+            }
+
+            if (this.sellIn < 6 && this.quality < 50) {
+                this.quality = this.quality + 1;
+            }
+        }
+
+        this.sellIn = this.sellIn - 1;
+
+        if (this.sellIn < 0) {
+            this.quality = 0;
+        }
+    }
 }
