0bf7db953c256d33ccef91771f958590d846fc08
aalberti <aa@antoinealberti.com>
2019-02-14 16:32:15 +0100

    override update in AgedBrie

--- Java/src/main/java/com/gildedrose/AgedBrie.java
+++ Java/src/main/java/com/gildedrose/AgedBrie.java
@@ -4,4 +4,18 @@ public class AgedBrie extends Item {
     public AgedBrie(int sellIn, int quality) {
         super("Aged Brie", sellIn, quality);
     }
+
+    @Override
+    void update() {
+        if (this.quality < 50) {
+            this.quality = this.quality + 1;
+        }
+
+        this.sellIn = this.sellIn - 1;
+
+        if (this.sellIn < 0 && this.quality < 50) {
+            this.quality = this.quality + 1;
+        }
+    }
+
 }
