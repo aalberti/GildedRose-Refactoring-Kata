a02387ed1f012b59ae9618f1035b8b7959072d2b
aalberti <aa@antoinealberti.com>
2019-02-15 15:05:45 +0100

    override nextDay in BackstagePasses

--- Java/src/main/java/com/gildedrose/BackstagePasses.java
+++ Java/src/main/java/com/gildedrose/BackstagePasses.java
@@ -4,4 +4,26 @@ class BackstagePasses extends Item {
     BackstagePasses(int sellIn, int quality) {
         super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
     }
+
+    @Override
+    void nextDay() {
+        if (this.quality < 50) {
+            this.quality = this.quality + 1;
+
+            if (this.sellIn < 11 && this.quality < 50) {
+                this.quality = this.quality + 1;
+            }
+
+            if (this.sellIn < 6 && this.quality < 50) {
+                this.quality = this.quality + 1;
+            }
+        }
+
+        this.sellIn = this.sellIn - 1;
+
+        if (this.sellIn < 0) {
+            this.quality = 0;
+        }
+    }
+
 }
