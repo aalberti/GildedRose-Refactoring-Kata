1a208d1826a270073b3f14750e80eb5db1bd638d
aalberti <aa@antoinealberti.com>
2019-02-15 15:07:18 +0100

    remove Backstage passes from Item.nextDay

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -15,39 +15,23 @@ public class Item {
     }
 
     void nextDay() {
-        if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (this.quality > 0 && !this.name.equals("Sulfuras, Hand of Ragnaros")) {
-                this.quality = this.quality - 1;
-            }
-        } else {
-            if (this.quality < 50) {
-                this.quality = this.quality + 1;
-
-                if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (this.sellIn < 11 && this.quality < 50) {
-                        this.quality = this.quality + 1;
-                    }
-
-                    if (this.sellIn < 6 && this.quality < 50) {
-                        this.quality = this.quality + 1;
-                    }
-                }
-            }
+        if (this.quality > 0 && !isSulfuras()) {
+            this.quality = this.quality - 1;
         }
 
-        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (!isSulfuras()) {
             this.sellIn = this.sellIn - 1;
         }
 
         if (this.sellIn < 0) {
-            if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (this.quality > 0 && !this.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    this.quality = this.quality - 1;
-                }
-            } else {
-                this.quality = 0;
+            if (this.quality > 0 && !isSulfuras()) {
+                this.quality = this.quality - 1;
             }
         }
+    }
+
+    private boolean isSulfuras() {
+        return this.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
     @Override
